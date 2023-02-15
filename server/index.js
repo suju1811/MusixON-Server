c8dad63ef3772883838a87ac55ee2ef3630cfd48
Siddharth <sidyyadav7899@gmail.com>
2023-02-15 22:26:26 +0530

    musixon server updated

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({limit:"30mb",extended : "true"}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:"true"}));
 app.use(cookieParser());
 app.use(cors({
-    origin:["https://transcendent-toffee-127b69.netlify.app"],
+    origin:["https://musixon-coffee.netlify.app"],
     methods:["POST","GET","PUT"],
     credentials:true
 }));
